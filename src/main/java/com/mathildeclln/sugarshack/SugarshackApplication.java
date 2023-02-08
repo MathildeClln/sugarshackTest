aa7561adeccabd8236693aaa44bf65a5b0a846a6
Mathilde Collain <mathilde.collain@gmail.com>
2023-02-08 14:49:40 -0500

    First commit

---
+++ src/main/java/com/mathildeclln/sugarshack/SugarshackApplication.java
@@ -0,0 +1,13 @@
+package com.mathildeclln.sugarshack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarshackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugarshackApplication.class, args);
+	}
+
+}
