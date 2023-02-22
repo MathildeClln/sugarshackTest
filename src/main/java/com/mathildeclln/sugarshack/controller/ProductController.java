b16316aa711a77a821dfac24e6c27e637ba0cc39
Mathilde Collain <mathilde.collain@gmail.com>
2023-02-22 18:34:23 -0500

    Merged CatalogueItemController and MapleSyrupController into ProductController.

--- src/main/java/com/mathildeclln/sugarshack/controller/ProductController.java
+++ src/main/java/com/mathildeclln/sugarshack/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.mathildeclln.sugarshack.controller;
 
+import com.mathildeclln.sugarshack.dto.CatalogueItemDto;
 import com.mathildeclln.sugarshack.dto.MapleSyrupDto;
+import com.mathildeclln.sugarshack.model.MapleType;
+import com.mathildeclln.sugarshack.service.CatalogueItemService;
 import com.mathildeclln.sugarshack.service.MapleSyrupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +11,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/products")
-public class MapleSyrupController {
+public class ProductController {
     @Autowired
-    private MapleSyrupService mapleServ;
+    private MapleSyrupService mapleSyrupService;
+    @Autowired
+    private CatalogueItemService catalogueItemService;
+
+    @GetMapping
+    public ArrayList<CatalogueItemDto> getCatalogue(MapleType type) {
+        return catalogueItemService.getCatalogue(type);
+    }
 
     @GetMapping("/{productId}")
     public MapleSyrupDto getProductInfo(@PathVariable String productId){
-        return mapleServ.getInfo(productId);
+        return mapleSyrupService.getInfo(productId);
     }
 }
